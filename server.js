7059aeb670e40e5cfbcb817a536b323b2f647289
samanthashleyrose <samantha.rose327@gmail.com>
2023-12-08 18:58:34 -0500

    Created options variable

--- server.js
+++ server.js
@@ -2,11 +2,29 @@ const mysql = require('mysql2');
 const inquirer = require('inquirer');
 
 const db = mysql.createConnection(
-    {
-      host: 'localhost',
-      user: 'root',
-      password: '',
-      database: 'employee_db'
-    },
-    console.log(`Connected to the employee_db database.`)
+  {
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'employee_db'
+  },
+  console.log(`Connected to the employee_db database.`)
 );
+
+const options = [
+  {
+    type: 'list',
+    message: "Select a license:",
+    name: 'license',
+    choices: [
+      'View All Employees',
+      'Add Employee',
+      'Update Employee Role',
+      'View All Roles',
+      'Add Role',
+      'View All Departments',
+      'Add Department',
+      'Quit'
+    ]
+  },
+];
