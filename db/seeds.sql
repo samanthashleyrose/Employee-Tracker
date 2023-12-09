a319749bc188d821db184f2849ad7fc723aa61e4
samanthashleyrose <samantha.rose327@gmail.com>
2023-12-09 15:23:20 -0500

    Changed manager to id instead of string of their name

--- db/seeds.sql
+++ db/seeds.sql
@@ -16,10 +16,10 @@ INSERT INTO roles (title, salary, department_id) VALUES
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES
 ('John', 'Doe', 1, NULL),
-('Mike', 'Chan', 2, 'John Doe'),
+('Mike', 'Chan', 2, 1),
 ('Ashley', 'Rodriguez', 3, NULL),
-('Kevin', 'Tupik', 4, 'Ashley Rodriguez'),
+('Kevin', 'Tupik', 4, 3),
 ('Kunal', 'Singh', 5, NULL),
-('Malia', 'Brown', 6, 'Kunal Singh'),
+('Malia', 'Brown', 6, 5),
 ('Sarah', 'Lourd', 7, NULL),
-('Tom', 'Allen', 8, 'Sarah Lourd');
+('Tom', 'Allen', 8, 7);
