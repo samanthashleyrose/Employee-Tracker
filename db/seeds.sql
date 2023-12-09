cdd143a0cc3890f01a238a0935f9479abf8634b7
samanthashleyrose <samantha.rose327@gmail.com>
2023-12-08 19:55:12 -0500

    Removed strings from around values in seeds.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -4,7 +4,7 @@ INSERT INTO department (names) VALUES
 ('Legal'),
 ('Sales');
 
-INSERT INTO roles ('title', 'salary', 'department_id') VALUES
+INSERT INTO roles (title, salary, department_id) VALUES
 ('Sales Lead', 100000, 4),
 ('Salesperson', 80000, 4),
 ('Lead Engineer', 150000, 1),
@@ -14,7 +14,7 @@ INSERT INTO roles ('title', 'salary', 'department_id') VALUES
 ('Legal Team Lead', 250000, 3),
 ('Lawyer', 190000, 3);
 
-INSERT INTO employee ('first_name', 'last_name', 'role_id', 'manager_id') VALUES
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES
 ('John', 'Doe', 1, NULL),
 ('Mike', 'Chan', 2, 'John Doe'),
 ('Ashley', 'Rodriguez', 3, NULL),
