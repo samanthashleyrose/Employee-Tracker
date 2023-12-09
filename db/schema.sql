b49f4e3bdbbe70a5a15df3b966a564a840ce00f5
samanthashleyrose <samantha.rose327@gmail.com>
2023-12-08 19:05:52 -0500

    Added comma

--- db/schema.sql
+++ db/schema.sql
@@ -19,6 +19,6 @@ CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
-    role_id INT NOT NULL
+    role_id INT NOT NULL,
     manager_id INT
 );
