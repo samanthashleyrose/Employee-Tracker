3cdbf620fa49f9b7684739b76b794f251a074679
samanthashleyrose <samantha.rose327@gmail.com>
2023-12-10 18:39:58 -0500

    Made functions async/await

--- server.js
+++ server.js
@@ -67,134 +67,124 @@ function init() {
 };
 
 // Function to View All Employees
-function viewAllEmployees() {
-  const query = `
-  SELECT 
-    employee.id, 
-    employee.first_name, 
-    employee.last_name, 
-    employee.role_id, 
-    CONCAT(manager.first_name, ' ', manager.last_name) AS manager
-  FROM employee
-  LEFT JOIN employee manager ON employee.manager_id = manager.id;`;
-  db.query(query, (err, results) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
+async function viewAllEmployees() {
+  try {
+    const query = `
+    SELECT 
+      employee.id, 
+      employee.first_name, 
+      employee.last_name, 
+      employee.role_id, 
+      CONCAT(manager.first_name, ' ', manager.last_name) AS manager
+    FROM employee
+    LEFT JOIN employee manager ON employee.manager_id = manager.id;`;
+    const [results] = await db.query(query);
     console.table(results);
     init();
-  })
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 // Function to Add Employee
-function addEmployee() {
-  const roles = getRoles();
-  const managers = getManagers();
+async function addEmployee() {
+  try {
+    const roles = await getRoles();
+    const managers = await getManagers();
 
-  const questions = [
-    {
-      type: 'input',
-      message: 'Enter employees first name',
-      name: 'firstNameForNewEmployee'
-    },
-    {
-      type: 'input',
-      message: 'Enter employees last name',
-      name: 'lastNameForNewEmployee'
-    },
-    {
-      type: 'list',
-      message: 'Select the employees role:',
-      name: 'roleForNewEmployee',
-      choices: roles
-    },
-    {
-      type: 'list',
-      message: 'Select the employees manager:',
-      name: 'managerForNewEmployee',
-      choices: managers
-    }
-  ];
-  inquirer
-    .prompt(questions)
-    .then((answers) => {
-      // Insert the employee into the database
-      const sql = 'INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)';
-      const values = [
-        answers.firstNameForNewEmployee,
-        answers.lastNameForNewEmployee,
-        answers.roleForNewEmployee,
-        answers.managerForNewEmployee,
-      ];
-      db.query(sql, values, (error) => {
-        if (error) {
-          console.error(error);
-          return;
-        }
+    const questions = [
+      {
+        type: 'input',
+        message: 'Enter employees first name',
+        name: 'firstNameForNewEmployee'
+      },
+      {
+        type: 'input',
+        message: 'Enter employees last name',
+        name: 'lastNameForNewEmployee'
+      },
+      {
+        type: 'list',
+        message: 'Select the employees role:',
+        name: 'roleForNewEmployee',
+        choices: roles
+      },
+      {
+        type: 'list',
+        message: 'Select the employees manager:',
+        name: 'managerForNewEmployee',
+        choices: managers
+      }
+    ];
 
-        console.log("Employee successfully added");
-        init();
-      })
-    });
+    const answers = await inquirer.prompt(questions);
+    // Insert the employee into the database
+    const sql = 'INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)';
+    const values = [
+      answers.firstNameForNewEmployee,
+      answers.lastNameForNewEmployee,
+      answers.roleForNewEmployee,
+      answers.managerForNewEmployee,
+    ];
+    await db.query(sql, values);
+
+    console.log("Employee successfully added");
+    init();
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 // Function to Update Employee Role
-function updateEmployeeRole() {
-  const employees = getEmployees();
-  const roles = getRoles();
+async function updateEmployeeRole() {
+  try {
+    const employees = await getEmployees();
+    const roles = await getRoles();
 
-  const questions = [
-    {
-      type: 'list',
-      message: "Select the employees you'd like to update:",
-      name: 'employeeToUpdate',
-      choices: employees
-    },
-    {
-      type: 'list',
-      message: 'Select the employees new role:',
-      name: 'employeesNewRole',
-      choices: roles
-    }
-  ];
+    const questions = [
+      {
+        type: 'list',
+        message: "Select the employees you'd like to update:",
+        name: 'employeeToUpdate',
+        choices: employees
+      },
+      {
+        type: 'list',
+        message: 'Select the employees new role:',
+        name: 'employeesNewRole',
+        choices: roles
+      }
+    ];
 
-  inquirer
-    .prompt(questions)
-    .then((answers) => {
-      // Update the employee's role in the database
-      const sql = 'UPDATE employee SET role_id = ? WHERE id = ?';
-      const values = [answers.newRoleId, answers.employeeToUpdate];
+    const answers = await inquirer.prompt(questions);
+    // Update the employee's role in the database
+    const sql = 'UPDATE employee SET role_id = ? WHERE id = ?';
+    const values = [answers.newRoleId, answers.employeeToUpdate];
+    await db.query(sql, values)
 
-      db.query(sql, values, (error) => {
-        if (error) {
-          console.error(error);
-          return;
-        }
-
-        console.log('Employee role successfully updated');
-        init();
-      })
-    });
+    console.log('Employee role successfully updated');
+    init();
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 // Function to Update Employee Role
-function viewAllRoles() {
-  const query = `
-  SELECT 
-    roles.id, 
-    roles.title, 
-    roles.salary, 
-    roles.department_id 
-  FROM roles`;
-  db.query(query, (err, results) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
+async function viewAllRoles() {
+  try {
+    const query = `
+    SELECT 
+      roles.id, 
+      roles.title, 
+      roles.salary, 
+      roles.department_id 
+    FROM roles`;
+    const [results, _] = await db.query(query);
     console.table(results);
     init();
-  })
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 // Helper function to get list of roles
