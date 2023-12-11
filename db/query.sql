2a56da816ee24b4fe2c9426895bb3afb7c8196e9
samanthashleyrose <samantha.rose327@gmail.com>
2023-12-10 19:58:20 -0500

    Updated query.sql

--- db/query.sql
+++ db/query.sql
@@ -1,2 +1,21 @@
 -- Get all employees
 SELECT * FROM employee;
+
+-- Get all roles
+SELECT * FROM roles;
+
+-- Get all departments
+SELECT * FROM department;
+
+-- Get employees with their roles and departments
+SELECT 
+    employee.id,
+    employee.first_name,
+    employee.last_name,
+    roles.title AS role_title,
+    department.names AS department,
+    CONCAT(manager.first_name, ' ', manager.last_name) AS manager
+FROM employee
+LEFT JOIN employee manager ON employee.manager_id = manager.id
+INNER JOIN roles ON employee.role_id = roles.id
+INNER JOIN department ON roles.department_id = department.id;
